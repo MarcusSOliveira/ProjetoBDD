39c261bfc725f27f3298376b6e0d8c0eb04c0962
Marcus Santos Oliveira <mso10br@gmail.com>
2016-03-23 10:15:28 -0300

    Added files via upload

---
+++ Projeto/Projeto.Web/Controllers/DepartamentoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto.Web.Models;
+using Projeto.Aplicacao.Contracts;
+using Projeto.Entidades;
+
+namespace Projeto.Web.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private IAppServiceDepartamento appDepartamento;
+
+        //ninject passado no contrutor para injeção de dependencia
+        public DepartamentoController( IAppServiceDepartamento appDepartamento)
+        {
+            this.appDepartamento = appDepartamento;
+        }
+
+        // GET: Departamento/Cadastro
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        // httpPost: Departamento/Cadastro
+        [HttpPost]
+        public ActionResult Cadastro(DepartamentoViewModelCadastro model)
+        {
+            if (ModelState.IsValid)
+            {
+                var d = new Departamento();
+                d.Nome = model.Nome;
+
+                try
+                {
+                    appDepartamento.Cadastrar(d);
+                    ViewBag.Mensagem = "Departamento cadastrado com sucesso";
+                    ModelState.Clear(); //Limpar a model
+
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Mensagem = e.Message;                    
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Projeto/Projeto.Web/Models/DepartamentoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Web.Models
+{
+    public class DepartamentoViewModelCadastro
+    {
+        [Display(Name = "Nome do Departamento")]
+        [Required(ErrorMessage = "Nome do Departamento Obrigatorio")]
+        public string Nome { get; set; }
+
+    }
+}
