2d8305ab729e56c8267ba8c8278ac379a9d6083c
Marcus Santos Oliveira <mso10br@gmail.com>
2016-03-23 09:42:05 -0300

    Added files via upload

---
+++ BDD/Projeto.Web.Tests/Features/ManterDepartamentos.feature
@@ -0,0 +1,17 @@
+﻿#language: pt-br 
+# O padrão é inglês, apague tudo que foi gerado e mude para a linha acima.
+
+Funcionalidade: Manter Departamento
+	Rotinas de cadastro, consulta, exclusão e edição de departamento
+
+Cenário: Cadastrar Departamento
+	Dado Acessar a pagina de cadastro de departamento
+	E Informar o nome "Departamento Teste"
+	Quando Solicitar o cadastro
+	Entao Sistema exibe mensagem "Departamento cadastrado com sucesso"
+
+Cenario: Validar dados ao cadastrar departamento
+	Dado Acessar a pagina de cadastro de departamento
+	E Nao preencher o nome do departamento
+	Quando Solicitar o cadastro
+	Entao Sistema exibe mensagem de erro "Nome do Departamento Obrigatorio"
---
+++ BDD/Projeto.Web.Tests/Steps/ManterDepartamentos.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox; 
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Html5;
+//using OpenQA.Selenium.IE;
+
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Projeto.Web.Tests.Steps
+{
+    [Binding] //Classe ser reconhecida pelo spec-flow
+    public class ManterDepartamentos
+    {
+        //Atributo para o selenium
+        private IWebDriver driver;
+
+        [Given(@"Acessar a pagina de cadastro de departamento")]
+        public void DadoAcessarAPaginaDeCadastroDeDepartamento()
+        {
+            //driver = new ChromeDriver();
+            //driver = new InternetExplorerDriver();
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://localhost:33310/departamento/cadastro");
+
+
+        }
+
+        [Given(@"Informar o nome ""(.*)""")]
+        public void DadoInformarONome(string param)
+        {// O nome vem do id da página
+            driver.FindElement(By.Id("nome")).SendKeys(param);
+        }
+
+        [When(@"Solicitar o cadastro")]
+        public void QuandoSolicitarOCadastro()
+        {
+            driver.FindElement(By.Id("btncadastro")).Click();
+        }
+
+        [Then(@"Sistema exibe mensagem ""(.*)""")]
+        public void EntaoSistemaExibeMensagem(string mensagem)
+        {
+            //capturar o conteudo da <div id='mensagem'>
+            Assert.AreEqual(mensagem, driver.FindElement(By.Id("mensagem")).Text);
+        }
+
+        [Given(@"Nao preencher o nome do departamento")]
+        public void DadoNaoPreencherONomeDoDepartamento()
+        {
+            driver.FindElement(By.Id("nome")).Clear();
+        }
+
+        [Then(@"Sistema exibe mensagem de erro ""(.*)""")]
+        public void EntaoSistemaExibeMensagemDeErro(string mensagem)
+        {
+            Assert.AreEqual(mensagem, driver.FindElement(By.Id("erronome")).Text);
+        }
+        
+    }
+}
